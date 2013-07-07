9cd2d69f55659a3f83e95c3a3332ea28fc3c7b96
gribbly <cameron.g.brown@gmail.com>
2013-07-07 20:27:53 +0000

    Added a bunch of patterns to play with (0-7) Moved tweak vars to top Minor cleanup

--- Flasher.py
+++ Flasher.py
@@ -1,6 +1,10 @@
 #attempt 5 - using queues... original source here:
 #http://stackoverflow.com/questions/375427/non-blocking-read-on-a-subprocess-pipe-in-python/437888#437888
 
+#tweaks
+nodes = 25
+pattern = 7 #starting pattern
+tick = 0.05 #starting tick
 fakeMode = False
 
 import sys
@@ -49,16 +53,96 @@ def randomPoint(ledStrip):
 			ledStrip.setPixel(i, [0, 0, 0])
 	ledStrip.update()
 	time.sleep(0)
+	
+def simpleChaser(ledStrip):
+	global sharedIndex
+	p = sharedIndex
+	sharedIndex += 1
+	if sharedIndex > ledStrip.nLeds - 1:
+		sharedIndex = 0
+	for i in range(0, ledStrip.nLeds):
+		if i == p:
+			ledStrip.setPixel(i,[255, 255, 255])
+		else:
+			ledStrip.setPixel(i, [0, 0, 0])
+	ledStrip.update()
+	time.sleep(0)
+	
+def cylonChaser(ledStrip):
+	global sharedBool
+	global sharedIndex
+
+	if sharedBool == False:
+		sharedIndex += 1
+	else:
+		sharedIndex -= 1
+
+	if sharedIndex > ledStrip.nLeds - 1:
+		sharedIndex = ledStrip.nLeds - 1
+		sharedBool = True
+	if sharedIndex < 0:
+		sharedIndex = 0
+		sharedBool = False
+
+	for i in range(0, ledStrip.nLeds):
+		if (i == sharedIndex):
+			ledStrip.setPixel(i,[255, 255, 255])
+		else:
+			ledStrip.setPixel(i, [0, 0, 0])
+	ledStrip.update()
+	time.sleep(0)
+	
+def stringBlink(ledStrip):
+	global sharedBool
+	b = sharedBool
+	for i in range(0, ledStrip.nLeds):
+		if b == True:
+			ledStrip.setPixel(i,[255, 255, 255])
+			sharedBool = False
+		else:
+			ledStrip.setPixel(i, [0, 0, 0])
+			sharedBool = True
+	ledStrip.update()
+	time.sleep(0)
+
+def lightningStringBlink(ledStrip, probability):
+	c = randint(0,100)
+	for i in range(0, ledStrip.nLeds):
+		if c < probability:
+			ledStrip.setPixel(i,[255, 255, 255])
+		else:
+			ledStrip.setPixel(i, [0, 0, 0])
+	ledStrip.update()
+	time.sleep(0)
+
+def stringPulsate(ledStrip):
+	global sharedBool
+	global sharedInt
+	if sharedBool == False:
+		sharedInt += 1
+	else:
+		sharedInt -= 1	
+
+	if sharedInt > 255:
+		sharedInt = 255
+		sharedBool = True
+	if sharedInt < 0:
+		sharedInt = 0
+		sharedBool = False	
+	
+	for i in range(0, ledStrip.nLeds):
+		ledStrip.setPixel(i,[sharedInt, sharedInt, sharedInt])
+	ledStrip.update()
+	time.sleep(0)	
 
 #START
 f = open('log','w')
 f.write(sys.argv[0] + ' HELLO WORLD @ {0}\n'.format(time.time()))
 
-pattern = 1
-tick = 0.05
-nodes = 25
-finger1 = 1.0
-finger2 = 1.0
+#globals (don't change these)
+sharedBool = False
+sharedIndex = 0
+sharedInt = 0
 
 if fakeMode == False:
 	f.write(sys.argv[0] + ' starting in REAL mode\n')
@@ -85,6 +169,8 @@ f.write(sys.argv[0] + ' - started SuperSimple.py @ {0}\n'.format(time.time()))
 
 print sys.argv[0] + ' tick is {0}'.format(tick)
 f.write(sys.argv[0] + ' - tick is {0}\n'.format(tick))
+print sys.argv[0] + ' pattern is {0}'.format(pattern)
+f.write(sys.argv[0] + ' - pattern is {0}\n'.format(pattern))
 
 nextTick = time.time()
 
@@ -120,7 +206,10 @@ while True:
 				f.write('Bad pattern input: ' + str(line) + '\n')
 			else:
 				print('Pattern update:' + str(pattern))
-				f.write('Pattern update: ' + str(pattern) + '\n')			
+				f.write('Pattern update: ' + str(pattern) + '\n')
+				sharedBool = False
+				sharedIndex = 0
+				sharedInt = 0
 
 	if time.time() > nextTick:
 		#f.write(sys.argv[0] + ' tick: {0}\n'.format(time.time()))
@@ -133,3 +222,13 @@ while True:
 			randomString(ledStrip)
 		elif pattern == 2:
 			randomPoint(ledStrip)
+		elif pattern == 3:
+			simpleChaser(ledStrip)
+		elif pattern == 4:
+			cylonChaser(ledStrip)
+		elif pattern == 5:
+			stringBlink(ledStrip)	
+		elif pattern == 6:			
+			lightningStringBlink(ledStrip, 33)
+		elif pattern == 7:
+			stringPulsate(ledStrip)
